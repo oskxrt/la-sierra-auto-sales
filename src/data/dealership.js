7514d75e0b4299e67fe21405e40cd6dbc5ffb460
Óscar Rincón <oskxrt@gmail.com>
2026-09-17 13:08:14 -0700

    feat: add dealership contact data

---
+++ src/data/dealership.js
@@ -0,0 +1,12 @@
+export const dealership = Object.freeze({
+  name: 'LA SIERRA AUTO SALES',
+  shortName: 'La Sierra Auto Sales',
+  phoneDisplay: '(702) 643-6329',
+  phoneHref: 'tel:+17026436329',
+  address: '4230 N Pecos Rd Ste 3, Las Vegas, NV 89115, United States',
+  directionsHref: 'https://www.google.com/maps/search/?api=1&query=4230%20N%20Pecos%20Rd%20Ste%203%2C%20Las%20Vegas%2C%20NV%2089115',
+  email: 'lasierraautosalesllc@gmail.com',
+  emailHref: 'mailto:lasierraautosalesllc@gmail.com',
+  faxDisplay: '(702) 643-6328',
+  hours: 'Monday – Saturday · 9:00 AM – 5:00 PM',
+});
